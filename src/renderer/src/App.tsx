88aa9f24aa8240db68dd17dcb1128f44efd92b44
Kyvrel <qiaocco@gmail.com>
2026-01-25 18:17:37 +0800

    feat: Add `@ai-sdk/react` and implement basic chat UI with message and input state.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,74 @@
+import { useState } from 'react'
+
 function App(): React.JSX.Element {
+  const [messages, setMessages] = useState([
+    { id: 1, role: 'ai', content: 'hello, how can i help you today' },
+    { id: 2, role: 'user', content: 'show me the design' }
+  ])
+
+  const [input, setInput] = useState('')
+
+  const handleClick = () => {
+    if (!input.trim()) return
+    const newUserMsg = { id: Date.now(), role: 'user', content: input }
+    setMessages([...messages, newUserMsg])
+    setInput('')
+  }
   return (
     <div className="flex h-screen bg-gray-50">
-      <h1 className="text-2xl">hello world</h1>
+      {/* sidebar */}
+      <div className="flex flex-col gap-4 w-64 bg-gray-50 p-4">
+        <button className="bg-blue-500 hover:bg-blue-600 rounded-md shadow-sm p-2  text-white">
+          + New Chat
+        </button>
+        <div className="flex flex-col flex-1 mt-4">
+          <div className="p-2 rounded hover:bg-gray-200 cursor-pointer text-sm text-gray-700">
+            Project Planning
+          </div>
+          <div className="p-2 rounded hover:bg-gray-200 cursor-pointer text-sm text-gray-700">
+            Python Script
+          </div>
+        </div>
+      </div>
+
+      {/* chat area */}
+      <div className="flex-1 flex flex-col h-full bg-white ">
+        <div className="flex-1 p-4 overflow-y-auto space-y-4">
+          {messages.map((message, _) => (
+            <div
+              key={`${message.id}`}
+              className={`flex ${message.role == 'ai' ? 'justify-start' : 'justify-end'} `}
+            >
+              <div
+                className={`${message.role == 'ai' ? ' bg-gray-100 text-gray-800' : 'bg-blue-500 text-white'} max-w-[80%] rounded-lg p-3`}
+              >
+                {message.content}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="h-16 p-4 flex justify-between gap-2 border-t border-t-gray-100">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key == 'Enter' && !e.shiftKey) {
+                e.preventDefault()
+                handleClick()
+              }
+            }}
+            placeholder="input your question"
+            className="px-2 py-1 w-full border border-gray-300 rounded-md"
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-600 rounded-md shadow-sm text-white px-2 py-1"
+            onClick={handleClick}
+          >
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
